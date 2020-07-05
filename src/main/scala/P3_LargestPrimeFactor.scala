8501a362cecfb603cd17086cad54151b32477757
Immodal <kychin.n@gmail.com>
2020-07-05 16:50:51 +1000

    Optimized P3 for HackerRank
    Still too slow for Hidden Task 5

--- src/main/scala/P3_LargestPrimeFactor.scala
+++ src/main/scala/P3_LargestPrimeFactor.scala
@@ -1,17 +1,20 @@
 object P3 {
   /**
+    * Modified version of LazyList.from() to return infinite LazyList[Long] from start with step
+    * https://github.com/scala/scala/blob/v2.12.8/src/library/scala/collection/immutable/Stream.scala#L1
+    */
+  def from(start: Long, step: Long): LazyList[Long] = LazyList.cons(start, from(start+step, step))
+
+  /**
+    * Sieve of Eratosthenes
+    * https://gist.github.com/motylwg/5868521#file-primesstreamexample-scala-L6
+    */
+  def sieve(s: LazyList[Long]): LazyList[Long] = s.head #:: sieve(s.tail filter(_ % s.head != 0))
+
+  /**
     * A LazyList of primes generated using the Sieve of Eratosthenes
     */
-  val primes: LazyList[Long] = {
-    // https://github.com/scala/scala/blob/v2.12.8/src/library/scala/collection/immutable/Stream.scala#L1
-    // Modified version of LazyList.from() to return infinite LazyList[Long] from start with step
-    def from(start: Long, step: Long): LazyList[Long] = LazyList.cons(start, from(start+step, step))
-    // Sieve of Eratosthenes
-    // https://gist.github.com/motylwg/5868521#file-primesstreamexample-scala-L6
-    def sieve(s: LazyList[Long]): LazyList[Long] = s.head #:: sieve(s.tail filter(_ % s.head != 0))
-
-    sieve(2 #:: from(3, 2))
-  }
+  val primes: LazyList[Long] = sieve(2 #:: from(3, 2))
 
   /**
     * Returns List of all prime factors of n, in descending order
@@ -20,7 +23,7 @@ object P3 {
     def pf(x:Long, i: Int, s: List[Long]): List[Long] = {
       if (x == 1) s
       else if (x % primes(i) == 0) pf(x/primes(i), i, primes(i) :: s)
-      else if (primes(i) <= Math.sqrt(n)) pf(x, i+1, s)
+      else if (primes(i+1) <= Math.sqrt(x)) pf(x, i+1, s)
       else x :: s
     }
 
